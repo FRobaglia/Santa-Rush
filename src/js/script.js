8cbb9ef78c5cf774bf8e607cc2f0ccaa8301b23c
unknown <felix.robaglia@gmail.com>
2018-12-17 17:28:54 +0100

    added bases for game

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+var speed = 1000;
+
+
+oxo.screens.loadScreen('game', function() {
+  console.log('loaded GAME');
+});
+setInterval()
+var character = document.getElementById('fireplace');
+oxo.animation.move(character, 'right', 10); // Move 10px to the right

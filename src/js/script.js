9e0215ad26a0d8cae6a121e1200b28bc40049c7d
unknown <felix.robaglia@gmail.com>
2018-12-17 17:37:13 +0100

    added js

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,17 @@
-var speed = 1000;
+var speed = 100;
+var santa = document.getElementById("santa");
+var fireplace = document.getElementById("fireplace");
 
+turnInterval = setInterval(turn, speed); // Call the turn function periodically
 
-oxo.screens.loadScreen('game', function() {
-  console.log('loaded GAME');
+oxo.screens.loadScreen("game", function() {
+  console.log("loaded GAME");
 });
-setInterval()
-var character = document.getElementById('fireplace');
-oxo.animation.move(character, 'right', 10); // Move 10px to the right
+
+function turn() {
+  oxo.animation.move(fireplace, "left", 10); // Move 10px to the right
+  console.log("debug");
+}
+
+
+turn();

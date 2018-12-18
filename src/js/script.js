4264faf925060dcd6a94a5eed722536f13b95b6f
unknown <felix.robaglia@gmail.com>
2018-12-18 12:12:28 +0100

    jump

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ var santaSpeed = 10;
 var stageSpeed = 10;
 var direction = "left";
 var directionDown = "down";
-var gravity = 1;
+var gravity = 3;
 var size = 1;
 var santa;
 var fireplace;
@@ -25,6 +25,15 @@ oxo.inputs.listenKeyOnce("enter", function() {
   }
 });
 
+function jump() {
+  if (gravity > 0) {
+    gravity = -gravity;
+    setTimeout(function() {
+      gravity = -gravity;
+    }, 1000);
+  }
+}
+
 function player() {
   oxo.animation.move(santa, directionDown, gravity, true);
 }
@@ -35,18 +44,19 @@ function stage() {
 function addFireplace() {
   // Add a bonus element to the screen at a random position
   var randomFireplace = oxo.elements.createElement({
-    class: '.stage__fireplace',
+    class: ".stage__fireplace",
     styles: {
       transform:
-        'translate(' +
+        "translate(" +
         oxo.utils.getRandomNumber(0, xSquares - 1) * size +
-        'px, '
-    },
+        "px, "
+    }
   });
   oxo.elements.onCollisionWithElementOnce(santa, randomFireplace, function() {
-    console.log('nique ta mere')
+    console.log("nique ta mere");
   });
 }
+
 oxo.inputs.listenKey("up", function() {
-  oxo.animation.move(santa, "up", 100, true);
+  jump();
 });
